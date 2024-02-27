793b721a1ef7cdb773f33dc968cbbe457a8c4ddd
felixgaschi <felix@posos.fr>
2024-02-27 13:52:35 +0100

    feat(segmenter): add thread to show segmenter errors

--- multilingual_eval/tokenization/chinese_segmenter.py
+++ multilingual_eval/tokenization/chinese_segmenter.py
@@ -3,17 +3,16 @@ from time import sleep
 import os
 from subprocess import Popen, PIPE, DEVNULL
 from nltk.parse.corenlp import CoreNLPParser
-import threading
 import sys
 import logging
+import threading
 
 
 def filtered_stderr_logger(p):
     for line in p.stderr:
-        text = line.decode()
-        if len(text.split()) > 2 and text.split()[1] == "INFO":
+        if len(line.split()) > 2 and line.split()[1] == "INFO":
             continue
-        print(text, file=sys.stderr)
+        print(f"Stanford Segmenter subprocess: {line.strip()}", file=sys.stderr)
 
 
 class StanfordSegmenter:
@@ -52,7 +51,7 @@ class StanfordSegmenter:
                 logging.error(f"Got HTTPError with following sentence: {repr(part)}")
                 raise e
             tokens += [
-                token["originalText"] or token["word"]
+                token["originalline"] or token["word"]
                 for sentence in result["sentences"]
                 for token in sentence["tokens"]
             ]
@@ -66,10 +65,12 @@ class StanfordSegmenter:
                     f"tools/stanford-corenlp-full-2016-10-31 does not exist, please install the Stanford Segmenter (download_resources/stanford_tokenizer.sh)"
                 )
             self.server_process = Popen(
-                ["/bin/bash", "subscripts/launch_corenlp_server.sh", str(self.port)], stderr=sys.stderr, stdout=DEVNULL
+                ["/bin/bash", "subscripts/launch_corenlp_server.sh", str(self.port)], stderr=PIPE, stdout=DEVNULL, universal_newlines=True
             )
 
             self.segmenter = CoreNLPParser(f"http://localhost:{self.port}", encoding="utf8")
+            self.log_thread = threading.Thread(target=filtered_stderr_logger, args=(self.server_process,), daemon=True)
+            self.log_thread.start()
             sleep(2)
             self.start_and_wait_for_availability()
 
@@ -80,12 +81,12 @@ class StanfordSegmenter:
             self.segmenter.api_call(
                 "只是教授和警察双方都对不尊重的暗示表现得过于敏感", {"annotators": "tokenize,ssplit"}, timeout=1
             )
-        except (requests.exceptions.ConnectionError, ConnectionRefusedError) as e:
+        except (requests.exceptions.ConnectionError, ConnectionRefusedError, requests.exceptions.HTTPError) as e:
             sleep(wait)
             self.start_and_wait_for_availability(max_iter=max_iter - 1, wait=wait * 2)
 
     def cleanup(self):
         if self.server_process is not None:
             self.server_process.terminate()
-
+            self.log_thread = None
         self.segmenter = None
