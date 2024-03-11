e5400fbb40ee30bdc2cb9fcf08f7005b28dd2d74
felixgaschi <felix@posos.fr>
2024-03-11 13:30:08 +0100

    feat(figures): finish loss_by_layer script

--- scripts/2024_emnlp/generate_figures/loss_by_layer_freezing.py
+++ scripts/2024_emnlp/generate_figures/loss_by_layer_freezing.py
@@ -1,8 +1,16 @@
 import os
+import sys
 import json
 import pandas as pd
 import itertools
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append(os.curdir)
+
+from multilingual_eval.plotting_utils import COLOR_PALETTE, get_statistics, plot_with_statistics
+
 
 if __name__ == "__main__":
 
@@ -22,7 +30,7 @@ if __name__ == "__main__":
     if not os.path.isdir(output_dir):
         Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    methods = ["during_partial_freeze"]
+    methods = ["during_partial_freeze", "freeze_realign_unfreeze"]
     aligners = ["awesome", "fastalign", "dico"]
 
     langs = ["ar", "es", "fr", "zh", "ru"]
@@ -45,12 +53,74 @@ if __name__ == "__main__":
     print(df.columns)
 
     for method, aligner in itertools.product(methods, aligners):
+        c_idx = 0
         for i in range(args.n_layers):
+
             subdf = df[df.method == f"{method}_0_{i}_{aligner}"]
             if len(subdf) == 0:
                 continue
-
             
+            
+            task_loss = list(subdf["task_loss"])
+            train_loss = list(subdf["train_loss"])
 
+            task_loss = np.array(task_loss)
+            train_loss = np.array(train_loss)
+
+            print(task_loss.shape, train_loss.shape)
+            if len(task_loss.shape) < 2 or len(train_loss.shape) < 2 or task_loss.shape[0] != train_loss.shape[0] or train_loss.shape[1] < task_loss.shape[1]:
+                continue
+            train_loss = train_loss[:,-task_loss.shape[1]:]
+
+            realignment_loss = train_loss - task_loss
+
+            plot_with_statistics(
+                list(range(realignment_loss.shape[1])),
+                *get_statistics(task_loss.T),
+                color=COLOR_PALETTE[c_idx],
+                legend=f"task loss {i}",
+                alpha=0.01
+            )
+            plot_with_statistics(
+                list(range(realignment_loss.shape[1])),
+                *get_statistics(realignment_loss.T),
+                color=COLOR_PALETTE[c_idx],
+                legend=f"align loss {i}",
+                linestyle="dashed",
+                alpha=0.01
+            )
+            c_idx += 1
+        plt.legend()
+        plt.ylim((0,0.5))
+        plt.xlabel("log steps")
+        plt.ylabel("loss")
+        plt.savefig(os.path.join(output_dir, f"{method}_{aligner}.png"))
+        plt.clf()
+
+        for subset in langs + ["avg", "same"]:
+            c_idx = 0
+            for i in range(args.n_layers):
+
+                subdf = df[df.method == f"{method}_0_{i}_{aligner}"]
+                if len(subdf) == 0:
+                    continue
+
+                eval_subset_accuracy = np.array(list(subdf[f"eval_{subset}_accuracy"]))
+
+                plot_with_statistics(
+                    list(range(eval_subset_accuracy.shape[1])),
+                    *get_statistics(eval_subset_accuracy.T),
+                    color=COLOR_PALETTE[c_idx],
+                    legend=f"layer {i}",
+                    alpha=0.1
+                )
+
+                c_idx += 1
+
+            plt.legend()
+            plt.xlabel("log steps")
+            plt.ylabel("accuracy")
+            plt.savefig(os.path.join(output_dir, f"{method}_{aligner}_{subset}_accuracy.png"))
+            plt.clf()
 
 
