6efb3d72692206c9661a66b3b2a5be19c7e8b4eb
Félix Gaschi <flx.gsch@gmail.com>
2024-03-25 20:38:33 +0000

    feat(udpos): include 34 languages

--- multilingual_eval/datasets/xtreme_udpos.py
+++ multilingual_eval/datasets/xtreme_udpos.py
@@ -16,24 +16,43 @@ get_xtreme_udpos = get_token_classification_getter(
 )
 
 
-
+# missing: bn, mk, ms
 wuetal_subsets = {
     "en": "en_ewt",
+    "af": "af_afribooms",
     "ar": "ar_padt",
     "bg": "bg_btb",
+    "ca": "ca_ancora",
+    "cs": "cs_pdt",
+    "da": "da_ddt",
     "de": "de_gsd",
     "el": "el_gdt",
     "es": "es_gsd",
+    "fa": "fa_perdt",
+    "fi": "fi_pud",
     "fr": "fr_gsd",
-    "hi": "hi_pud",
+    "he": "he_htb",
+    "hi": "hi_hdtb",
+    "hu": "hu_szeged",
+    "it": "it_isdt",
+    "ja": "ja_gsd",
+    "ko": "ko_gsd",
+    "lv": "lv_lvtb",
+    "lt": "lt_alksnis",
+    "no": "no_bokmaal",
+    "pl": "pl_pdb",
+    "pt": "pt_gsd",
+    "ro": "ro_rrt",
     "ru": "ru_gsd",
+    "sk": "sk_snk",
+    "sl": "sl_ssj",
+    "sv": "sv_pud",
+    "ta": "ta_ttb",
     "th": "th_pud",
     "tr": "tr_pud",
+    "uk": "uk_iu",
     "vi": "vi_vtb",
     "zh": "zh_gsd",
-    "af": "af_afribooms",
-    "fa": "fa_perdt",
-    "hi": "hi_hdtb"
 }
 
 get_wuetal_udpos = get_token_classification_getter(
